6ac360423bd5ff1f7ae02f1b088263b0860ee363
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2020-12-18 22:04:58 +0530

    changed the port number

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/", async (req, res) => {
 app.use("/game", require("./routes/game"));
 
 //Declaring the server
-var port = 3001;
+var port = 5000;
 app.listen(process.env.PORT || port, () =>
   console.log(`Server started on ${port}`)
 );
