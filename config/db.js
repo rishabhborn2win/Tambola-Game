d8a87599dbd1d8d7fdf1cb57c5b2f15b7ad3b35d
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-02-01 10:08:30 +0530

    removed dbURI

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = process.env.mongoURI;
+const db = process.env.mongoURI || config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect("mongodb+srv://rishu:12345@devconnector.ao3hi.mongodb.net/tambola?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
