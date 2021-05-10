b29579ca034b9de97a86539e53d5d817048cec09
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-05-10 22:05:35 +0530

    removed dev tools extension from the app

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 
 const initialState = {};
 
-const middleware = [thunk];
+let middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeWithDevTools( applyMiddleware(...middleware) )
 );
 
 export default store;
+
