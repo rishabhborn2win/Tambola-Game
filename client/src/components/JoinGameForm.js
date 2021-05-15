6db7c717ce1cad0ccb8fb1579329b0b9c5cf1b78
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-05-15 16:46:32 +0530

    Revert "removed unwanted dependencies"
    This reverts commit ff7143833c36136a4189603555d0d76ed3dbae92.

--- client/src/components/JoinGameForm.js
+++ client/src/components/JoinGameForm.js
@@ -1,5 +1,3 @@
-
-import React from 'react'
 import { Fragment, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
@@ -29,7 +27,7 @@ const JoinGameForm = ({ match, notifyFill, joinGame, game }) => {
       notifyFill("Player Name Should have more then 6 Char");
     } else {
       joinGame(playername, gameID);
-      return (<Redirect to="/play" />);
+      <Redirect to="/play" />;
     }
   };
   return (
